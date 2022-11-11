0d9ae73e1a6d640ed3f69c2b4c373130b5c90af4
mabushelbaia <mabushelbaia@gmail.com>
2022-11-11 16:15:32 +0200

    Solved Shell Question for the 3rd Semester 2021-2022

---
+++ Linux-Finals/3rd Semester 2021-2022/shell.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+echo "Enter file name: "
+read file
+if [ ! -e "$file" ]; then # Checking if file is accessible 
+	echo "File doesn't exist"
+	exit 
+fi
+max=0
+maxword=""
+line=0
+for word in `cat $file`; do 
+ 	n=$(echo "$word" | grep -o -i '[aioue]' | wc -l)
+	if [ "$n" -gt "$max" ]; then
+		max=$n
+		maxword=$word
+		line=$(grep -n "$word" $file | cut -d: -f1)
+	fi
+done
+echo "The word with maximum vowels is \"$maxword\" on line $line with $max vowels."
+echo "do you want to enter another file? (y/n)"
+read choice
+if [ "$choice" = "y" ]; then
+	./shell.sh
+fi
